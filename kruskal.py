be77a6b529e5f9b329b29f2afeba9511d9c975c4
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-05-28 10:00:33 +0530

    Update kruskal.py

--- kruskal.py
+++ kruskal.py
@@ -1,10 +1,31 @@
 """
 Greedy Algorithm
+
 The greedy method is one of the strategies like Divide and conquer used to solve the problems. This method is used for solving optimization problems. An optimization problem is a problem that demands either maximum or minimum results. Let's understand through some terms.
 
 The Greedy method is the simplest and straightforward approach. It is not an algorithm, but it is a technique. The main function of this approach is that the decision is taken on the basis of the currently available information. Whatever the current information is present, the decision is made without worrying about the effect of the current decision in future.
 
 This technique is basically used to determine the feasible solution that may or may not be optimal. The feasible solution is a subset that satisfies the given criteria. The optimal solution is the solution which is the best and the most favorable solution in the subset. In the case of feasible, if more than one solution satisfies the given criteria then those solutions will be considered as the feasible, whereas the optimal solution is the best solution among all the solutions.
+
+Kruskal's Algorithm
+
+Spanning tree - A spanning tree is the subgraph of an undirected connected graph.
+Minimum Spanning tree - Minimum spanning tree can be defined as the spanning tree in which the sum of the weights of the edge is minimum. The weight of the spanning tree is the sum of the weights given to the edges of the spanning tree.
+
+Kruskal's Algorithm is used to find the minimum spanning tree for a connected weighted graph. The main target of the algorithm is to find the subset of edges by using which we can traverse every vertex of the graph. It follows the greedy approach that finds an optimum solution at every stage instead of focusing on a global optimum.
+
+Algorithm
+
+Step 1: Create a forest F in such a way that every vertex of the graph is a separate tree.  
+Step 2: Create a set E that contains all the edges of the graph.  
+Step 3: Repeat Steps 4 and 5 while E is NOT EMPTY and F is not spanning  
+Step 4: Remove an edge from E with minimum weight  
+Step 5: IF the edge obtained in Step 4 connects two different trees, then 
+		add it to the forest F (for combining two trees into one tree).  
+	ELSE
+		Discard the edge  
+Step 6: END
+
 """
 # Python program for Kruskal's algorithm to find Minimum Spanning Tree of a given connected, undirected and weighted graph
 from collections import defaultdict
