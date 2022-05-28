b2dffd2b8a4777640931d7190b4dc1e63ab282bf
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-05-28 12:38:48 +0530

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,3 +1,7 @@
+"""
+Introduction to Transposition Techniques
+The transposition technique is a cryptographic technique that converts the plain text to cipher text by performing permutations on the plain text, i.e., changing each character of plain text for each round. It includes various techniques like the Rail Fence technique, Simple columnar transposition technique, simple columnar transposition technique with multiple rounds, Vernam cipher, and book Cipher to encrypt the plain text in a secure way.
+"""
 def split_len(seq, length):
    return [seq[i:i + length] for i in range(0, len(seq), length)]
 def encode(key, plaintext):
